14d16e22cec16f0dfc3787ce2981bac492f2197e
Deepak <deepak7mahto@gmail.com>
2019-06-01 11:55:39 +0530

    espresso test added for automated ui flow testing

--- app/src/androidTest/java/com/dpk/githubtrendingrepositories/MainActivityTest.java
+++ app/src/androidTest/java/com/dpk/githubtrendingrepositories/MainActivityTest.java
@@ -1,5 +1,10 @@
 package com.dpk.githubtrendingrepositories;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,6 +15,7 @@ import com.dpk.githubtrendingrepositories.main.MainPresenter;
 import com.dpk.githubtrendingrepositories.model.Repo;
 import com.dpk.githubtrendingrepositories.model.RepoDetails;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,14 +35,27 @@ public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    private Repo[] repos;
+    private MainMvpView mainMvpView;
+    private MainPresenter mainPresenter;
+
+    @Before
+    public void setUp() {
+        repos = new Repo[]{new Repo("testUsername", "testName", "testType", "testUrl", "https://google.com", new RepoDetails("testRepoName", "testDescription", "testUrl"))};
+        mainMvpView = Mockito.mock(MainMvpView.class);
+        mainPresenter = new MainPresenter(mainMvpView, new MainInteractor());
+    }
 
     @Test
     public void testShowRepo() {
-        Repo[] repos = new Repo[]{new Repo("testUsername", "testName", "testType", "testUrl", "https://google.com", new RepoDetails("testRepoName", "testDescription", "testUrl"))};
-        MainMvpView mainMvpView = Mockito.mock(MainMvpView.class);
-        MainPresenter mainPresenter = new MainPresenter(mainMvpView, new MainInteractor());
         mainPresenter.loadTestData(repos);
         Mockito.verify(mainMvpView, Mockito.atLeastOnce()).showRepoList(repos);
+
+        Espresso.onView(ViewMatchers.withId(R.id.reposRecyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
+
+        Espresso.onView(ViewMatchers.withId(R.id.repoNameTextView)).check(ViewAssertions.matches(ViewMatchers.withId(R.id.repoNameTextView)));
+
     }
 
 }
--- app/src/main/java/com/dpk/githubtrendingrepositories/main/MainActivity.java
+++ app/src/main/java/com/dpk/githubtrendingrepositories/main/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import com.dpk.githubtrendingrepositories.R;
 import com.dpk.githubtrendingrepositories.base.BaseActivity;
 import com.dpk.githubtrendingrepositories.model.Repo;
+import com.dpk.githubtrendingrepositories.model.RepoDetails;
 
 public class MainActivity extends BaseActivity implements MainMvpView {
 
@@ -20,7 +21,10 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         reposRecyclerView = findViewById(R.id.reposRecyclerView);
         reposRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mainMvpPresenter = new MainPresenter(this, new MainInteractor());
-        mainMvpPresenter.loadTrendingRepositories();
+//        mainMvpPresenter.loadTrendingRepositories();
+        Repo[] repos = new Repo[]{new Repo("testUsername", "testName", "testType", "testUrl", "https://google.com", new RepoDetails("testRepoName", "testDescription", "testUrl"))};
+
+        mainMvpPresenter.loadTestData(repos);
     }
 
     @Override
